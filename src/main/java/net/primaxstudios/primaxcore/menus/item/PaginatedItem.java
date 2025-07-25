f2a9b7ebf1500273fad0c8f91470546afb4caa63
ShadowXero <muhammadammad030@gmail.com>
2025-07-25 15:53:39 +0500

    Reverted back PaginatedItem

--- src/main/java/net/primaxstudios/primaxcore/menus/item/PaginatedItem.java
+++ src/main/java/net/primaxstudios/primaxcore/menus/item/PaginatedItem.java
@@ -1,52 +1,24 @@
 package net.primaxstudios.primaxcore.menus.item;
 
-import net.primaxstudios.primaxcore.events.menu.CustomMenuClickEvent;
 import net.primaxstudios.primaxcore.menus.MenuHolder;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public interface PaginatedItem<T> extends MenuItem, ClickableItem {
+public interface PaginatedItem<T> extends MenuItem {
 
-    List<Integer> getSlots();
+    List<T> getPageItems(MenuHolder holder);
 
-    void setItem(MenuHolder holder, int slot, T object);
+    void setItem(MenuHolder holder, int slot, T item);
 
-    Map<Integer, T> getObjectBySlot(MenuHolder holder);
-
-    void setObjectBySlot(MenuHolder holder, Map<Integer, T> objectBySlot);
-
-    List<T> getObjects();
-
-    void onClick(CustomMenuClickEvent e, T object);
+    List<Integer> getDisplaySlots(MenuHolder holder);
 
     @Override
     default void setItem(MenuHolder holder) {
-        Map<Integer, T> objectBySlot = new HashMap<>();
+        List<T> items = getPageItems(holder);
+        List<Integer> slots = getDisplaySlots(holder);
 
-        List<T> objects = getObjects();
-        List<Integer> slots = getSlots();
-        for (int i = 0; i < Math.min(objects.size(), slots.size()); i++) {
-            T object = objects.get(i);
-            objectBySlot.put(i, object);
-            setItem(holder, slots.get(i), object);
+        for (int i = 0; i < Math.min(items.size(), slots.size()); i++) {
+            setItem(holder, slots.get(i), items.get(i));
         }
-
-        setObjectBySlot(holder, objectBySlot);
-    }
-
-    @Override
-    default void onClick(CustomMenuClickEvent e) {
-        MenuHolder holder = e.getHolder();
-        int slot = e.getOriginalEvent().getSlot();
-
-        Map<Integer, T> objectBySlot = getObjectBySlot(holder);
-        if (objectBySlot.isEmpty()) return;
-
-        T object = objectBySlot.get(slot);
-        if (object == null) return;
-
-        onClick(e, object);
     }
 }
