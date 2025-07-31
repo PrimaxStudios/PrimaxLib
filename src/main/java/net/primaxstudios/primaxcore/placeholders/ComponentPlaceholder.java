329a7aa49a50ebad49a279a8ba9d19fabffea812
ShadowX <muhammadammad1414@gmail.com>
2025-08-01 00:15:59 +0500

    ...
    Took 3 minutes

---
+++ src/main/java/net/primaxstudios/primaxcore/placeholders/ComponentPlaceholder.java
@@ -0,0 +1,15 @@
+package net.primaxstudios.primaxcore.placeholders;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @Setter
+public class ComponentPlaceholder {
+
+    //TODO
+
+    private final Map<String, String> replacementByPlaceholder = new HashMap<>();
+}
